c59fd598af30748b3893dcedecc7002a06661f0d
Ahron Henrique Helfenstein <ahron.helfenstein@gmail.com>
2018-10-03 21:59:39 -0300

    Generated with JHipster 5.3.4

---
+++ src/main/java/br/com/cwi/crescer/repository/BlogRepository.java
@@ -0,0 +1,19 @@
+package br.com.cwi.crescer.repository;
+
+import br.com.cwi.crescer.domain.Blog;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Spring Data  repository for the Blog entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+
+    @Query("select blog from Blog blog where blog.user.login = ?#{principal.username}")
+    List<Blog> findByUserIsCurrentUser();
+
+}
---
+++ src/main/java/br/com/cwi/crescer/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package br.com.cwi.crescer.web.rest.vm;
